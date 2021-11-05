48bd2b2d5173537b58123dffaba0ce730403decb
Zeyu-Li <zeyu.li23@gmail.com>
2021-11-04 19:09:13 -0600

    initialized

--- js/index.js
+++ js/index.js
@@ -1,41 +1,56 @@
 class CelestialObject {
-	constructor(mass, initialPosition, initialVelocity) {
-		this.mass = mass;
-		this.old_position = initialPosition;
-		this.old_velocity = initialVelocity;
-	}
+  constructor(mass, initialPosition, initialVelocity) {
+    this.mass = mass;
+    this.old_position = initialPosition;
+    this.old_velocity = initialVelocity;
+  }
 
-	get position() {
-		const position_incr = this.velocity.multiply(TIME_SCALE);
-		this.old_position = position_incr.add(this.old_position);
-		return this.old_position;
-	}
+  get position() {
+    const position_incr = this.velocity.multiply(TIME_SCALE);
+    this.old_position = position_incr.add(this.old_position);
+    return this.old_position;
+  }
 
-	get velocity() {
-		const velocity_incr = this.acceleration.multiply(TIME_SCALE);
-		this.old_velocity = velocity_incr.add(this.old_velocity);
-		return this.old_velocity;
-	}
+  get velocity() {
+    const velocity_incr = this.acceleration.multiply(TIME_SCALE);
+    this.old_velocity = velocity_incr.add(this.old_velocity);
+    return this.old_velocity;
+  }
 
-	get acceleration() {
-		return new Vector(0, 0);
-	}
+  get acceleration() {
+    return new Vector(0, 0);
+  }
 }
 
+let canvas, ctx;
+
 function fitToScreen() {
-	canvas.width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-	canvas.height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+  canvas.width = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  );
+  canvas.height = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+  );
 }
 
 function renderBackground() {
-	ctx.fillStyle = "#000";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#191529";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
+
+const initialize = () => {
+  // get canvas and context
+  canvas = document.getElementById("game-canvas");
+  ctx = canvas.getContext("2d");
+};
 
 function render() {
-	fitToScreen();
-	renderBackground();
-	requestAnimationFrame(render);
+  fitToScreen();
+  renderBackground();
+  requestAnimationFrame(render);
 }
 
+initialize();
 render();
--- js/physics.js
+++ js/physics.js
@@ -1,60 +1,60 @@
-// TODO: finetune this constant
+// TODO: fine tune constant
 const G = 1;
-const TIME_SCALE = 1/60;
+const TIME_SCALE = 1 / 60;
 
 function isVector() {
-	return this.x !== undefined && this.y !== undefined;
+  return this.x !== undefined && this.y !== undefined;
 }
 
 class Vector {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-	add(o) {
-		if (typeof o === "number") {
-			return new Vector(this.x + o, this.y + o);
-		} else if (isVector(o)) {
-			return new Vector(this.x + o.x, this.y + o.y);
-		}
-		throw new TypeError("Not a Vector or Number");
-	}
+  add(o) {
+    if (typeof o === "number") {
+      return new Vector(this.x + o, this.y + o);
+    } else if (isVector(o)) {
+      return new Vector(this.x + o.x, this.y + o.y);
+    }
+    throw new TypeError("Not a Vector or Number");
+  }
 
-	subtract(o) {
-		if (typeof o === "number") {
-			return new Vector(this.x - o, this.y - o);
-		} else if (isVector(o)) {
-			return new Vector(this.x - o.x, this.y - o.y);
-		}
-		throw new TypeError("Not a Vector or Number");
-	}
+  subtract(o) {
+    if (typeof o === "number") {
+      return new Vector(this.x - o, this.y - o);
+    } else if (isVector(o)) {
+      return new Vector(this.x - o.x, this.y - o.y);
+    }
+    throw new TypeError("Not a Vector or Number");
+  }
 
-	multiply(o) {
-		if (typeof o === "number") {
-			return new Vector(this.x * o, this.y * o);
-		} else if (isVector(o)) {
-			return new Vector(this.x * o.x, this.y * o.y);
-		}
-		throw new TypeError("Not a Vector or Number");
-	}
+  multiply(o) {
+    if (typeof o === "number") {
+      return new Vector(this.x * o, this.y * o);
+    } else if (isVector(o)) {
+      return new Vector(this.x * o.x, this.y * o.y);
+    }
+    throw new TypeError("Not a Vector or Number");
+  }
 
-	distanceTo(o) {
-		if (!isVector(o)) {
-			throw new TypeError("Not a Vector")
-		}
-		const delta = this.subtract(o);
-		return Math.sqrt((delta.x**2) + (delta.y**2));
-	}
+  distanceTo(o) {
+    if (!isVector(o)) {
+      throw new TypeError("Not a Vector");
+    }
+    const delta = this.subtract(o);
+    return Math.sqrt(delta.x ** 2 + delta.y ** 2);
+  }
 }
 
 function GravityForce(o1, o2) {
-	const m1 = o1.mass;
-	const m2 = o2.mass;
-	const r = o1.position.subtract(o2.position);
-	const r_squared = r.multiply(r);
-	const GMm = G * m1 * m2;
-	const force_x = GMm / r.x;
-	const force_y = GMm / r.y;
-	return new Vector(force_x, force_y);
+  const m1 = o1.mass;
+  const m2 = o2.mass;
+  const r = o1.position.subtract(o2.position);
+  const r_squared = r.multiply(r);
+  const GMm = G * m1 * m2;
+  const force_x = GMm / r.x;
+  const force_y = GMm / r.y;
+  return new Vector(force_x, force_y);
 }
